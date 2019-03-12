84e7c4c0c8d9822603367e4855bf41342305e93d
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2019-03-12 15:00:22 +0200

    pure async version

--- index.ts
+++ index.ts
@@ -17,109 +17,161 @@ const dsGithubOwner = 'open-numbers';
 const dsGithubRepo = 'ddf--gapminder--systema_globalis';
 const isWin = /^win32/.test(os.platform());
 
-getLatestGithubTag(`github.com/${dsGithubOwner}/${dsGithubRepo}`, (tagError, tagVersion) => {
+(async function () {
+  const tagVersion = await getLatestGithubTag(`github.com/${dsGithubOwner}/${dsGithubRepo}`);
   const dsFeedUrl = dsFeedUrlTemp.replace('#version#', tagVersion);
   const tempPath = path.resolve('temp');
+  await removeDir(tempPath);
+  const dlFile = await download({ url: dsFeedUrl, path: tempPath, file: 'dl.zip' });
+  const unpackFun = isWin ? unpackWin : unpackNix;
+  const unpacked = await unpackFun({ fullPath: dlFile, target: path.resolve(tempPath, 'unpacked') });
+  const contentDir = await getFirstDir(unpacked);
+  const destDir = path.resolve('target');
+  await copy(contentDir, destDir);
+  await removeDir(tempPath);
+})();
 
-  download({
-    url: dsFeedUrl,
-    path: tempPath,
-    file: 'dl.zip'
-  }, (downloadErr, file) => {
-    const unpackFun = isWin ? unpackWin : unpackNix;
-
-    unpackFun({
-      fullPath: file,
-      target: path.resolve(tempPath, 'unpacked')
-    }, (unpackErr) => {
-      console.log(unpackErr);
-    });
-  });
-});
-
-function getLatestGithubTag(inputParam: string, onTagReady: Function) {
-  const input = inputParam.replace(/^(?!(?:https|git):\/\/)/, 'https://');
-  const tcp = net.connect({ host: urlLib.parse(input).host, port: 9418 });
-  const client = gitclient(input);
-  const tags = [];
-
-  client.refs.on('data', ref => {
-    const name = ref.name;
-
-    if (/^refs\/tags/.test(name)) {
-      tags.push(name.split('/')[2].replace(/\^\{\}$/, '').substr(1));
-    }
-  });
-
-  client
-    .pipe(transport(tcp))
-    .on('error', onTagReady)
-    .pipe(client)
-    .on('error', onTagReady)
-    .once('end', () => {
-      if (tags.length === 0) {
-        return onTagReady(Error('Tags are missing'));
+async function copy(source: string, dest: string): Promise<void> {
+  return new Promise((resolve: Function, reject: Function) => {
+    fse.copy(source, dest, err => {
+      if (err) {
+        return reject(err);
       }
 
-      onTagReady(null, semverSort.desc(tags)[0]);
+      resolve();
     });
-}
-
-function unpackWin(options, onUnpacked) {
-  const unzipper = new DecompressZip(options.fullPath);
-
-  unzipper.on('error', onUnpacked);
-  unzipper.on('extract', () => onUnpacked());
-
-  unzipper.extract({
-    path: path.resolve(options.target),
-    follow: true
   });
 }
 
-function unpackNix(options, onUnpacked) {
-  extract(options.fullPath, { dir: path.resolve(options.target) }, onUnpacked);
+async function getFirstDir(baseDir: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fs.readdir(baseDir, (err, files) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (files.length !== 1) {
+        return reject();
+      }
+
+      resolve(path.resolve(baseDir, files[0]));
+    });
+  });
+}
+
+async function getLatestGithubTag(inputParam: string): Promise<string> {
+  return new Promise((resolve: Function, reject: Function) => {
+    const input = inputParam.replace(/^(?!(?:https|git):\/\/)/, 'https://');
+    const tcp = net.connect({ host: urlLib.parse(input).host, port: 9418 });
+    const client = gitclient(input);
+    const tags = [];
+
+    client.refs.on('data', ref => {
+      const name = ref.name;
+
+      if (/^refs\/tags/.test(name)) {
+        tags.push(name.split('/')[2].replace(/\^\{\}$/, '').substr(1));
+      }
+    });
+
+    client
+      .pipe(transport(tcp))
+      .on('error', reject)
+      .pipe(client)
+      .on('error', reject)
+      .once('end', () => {
+        if (tags.length === 0) {
+          return reject('Tags are missing');
+        }
+
+        resolve(semverSort.desc(tags)[0]);
+      });
+  });
+}
+
+async function removeDir(what: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    fse.remove(what, err => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve();
+    });
+  });
+}
+
+async function unpackWin(options): Promise<string> {
+  const unzipper = new DecompressZip(options.fullPath);
+  const targetPath = path.resolve(options.target);
+
+  return new Promise((resolve: Function, reject: Function) => {
+    unzipper.on('error', reject);
+    unzipper.on('extract', () => {
+      resolve(targetPath);
+    });
+
+    unzipper.extract({
+      path: targetPath,
+      follow: true
+    });
+  });
+}
+
+async function unpackNix(options): Promise<string> {
+  const targetPath = path.resolve(options.target);
+
+  return new Promise((resolve: Function, reject: Function) => {
+    extract(options.fullPath, { dir: targetPath }, err => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(targetPath);
+    });
+  });
 };
 
-function download(options, onDownloadCompleted) {
-  const TIMEOUT = 240000;
-  const mode = parseInt('0777', 8) & (~process.umask());
+async function download(options): Promise<string> {
+  return new Promise((resolve: Function, reject: Function) => {
+    const TIMEOUT = 240000;
+    const mode = parseInt('0777', 8) & (~process.umask());
 
-  fs.mkdir(options.path, mode, pathErr => {
-    if (pathErr) {
-      onDownloadCompleted(pathErr);
-      return;
-    }
+    fs.mkdir(options.path, mode, pathErr => {
+      if (pathErr) {
+        return reject(pathErr);
+      }
 
-    const filePath = path.resolve(options.path, options.file);
-    const file = fs.createWriteStream(filePath);
-    const timeoutWrapper = req => () => {
-      req.abort();
-      onDownloadCompleted('File transfer timeout!');
-    };
+      const filePath = path.resolve(options.path, options.file);
+      const file = fs.createWriteStream(filePath);
+      const timeoutWrapper = req => () => {
+        req.abort();
+        reject('File transfer timeout!');
+      };
 
-    const relatedLib = options.url.indexOf('https://') === 0 ? https : http;
-    const request = relatedLib.get(options.url)
-      .on('response', res => {
-        res.on('data', chunk => {
-          file.write(chunk);
+      const relatedLib = options.url.indexOf('https://') === 0 ? https : http;
+      const request = relatedLib.get(options.url)
+        .on('response', res => {
+          res.on('data', chunk => {
+            file.write(chunk);
 
-          clearTimeout(timeoutId);
-          timeoutId = setTimeout(timeoutAction, TIMEOUT);
-        }).on('end', () => {
-          clearTimeout(timeoutId);
-          file.end();
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(timeoutAction, TIMEOUT);
+          }).on('end', () => {
+            clearTimeout(timeoutId);
+            file.end();
 
-          onDownloadCompleted(null, filePath);
-        }).on('error', err => {
-          clearTimeout(timeoutId);
+            resolve(filePath);
+          }).on('error', err => {
+            clearTimeout(timeoutId);
 
-          onDownloadCompleted(err.message);
+            reject(err);
+          });
         });
-      });
 
-    const timeoutAction = timeoutWrapper(request);
+      const timeoutAction = timeoutWrapper(request);
 
-    let timeoutId = setTimeout(timeoutAction, TIMEOUT);
+      let timeoutId = setTimeout(timeoutAction, TIMEOUT);
+    });
   });
 }
